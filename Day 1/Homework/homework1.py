0505148ed69070435c7cfbd3a6f2c8d84bde93a7
halfbloodxru <gio.ruski.1997@gmail.com>
2024-06-24 17:43:06 +0400

    second commit

--- Day 1/Homework/homework1.py
+++ Day 1/Homework/homework1.py
@@ -3,4 +3,5 @@ name = "Giorgi"
 
 surname = "Salia"
 age = 24
-print("My name is" + " " + name + " " + surname + "," + " " + "but everybody calls me Xrusha." + " " + "I am" + " " + str(age) + " " + "and I`m studying at GOAL-Oriented Academy" )
+print("Hello, friends")
+print("My name is",  name,  surname + ", but everybody calls me \"Xrusha\".", "I am", str(age), "and I`m studying at GOAL-Oriented Academy" )
