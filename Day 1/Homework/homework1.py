83d61cf007b740534a5f6fa72a460678c8f4523b
halfbloodxru <gio.ruski.1997@gmail.com>
2024-06-24 15:46:57 +0400

    second comit

---
+++ Day 1/Homework/homework1.py
@@ -0,0 +1,6 @@
+name = "Giorgi"
+
+
+surname = "Salia"
+age = 24
+print("My name is" + " " + name + " " + surname + "," + " " + "but everybody calls me Xrusha." + " " + "I am" + " " + str(age) + " " + "and I`m studying at GOAL-Oriented Academy" )
